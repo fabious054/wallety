44485e94a659ff1f36469834dedb93f2b931659b
Fabio <fabio.h.abreu99@gmail.com>
2024-10-08 18:18:12 -0300

    using env

--- src/utils/Requests.js
+++ src/utils/Requests.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 export const login = async (username, password) => {
+    const API_URL = process.env.REACT_APP_API_URL;
     try {
-        const response = await axios.post('http://localhost:3000/login', {
-            email: username,
+        const response = await axios.post(`${API_URL}/login`, {
+            username: username,
             password: password,
         });
 
